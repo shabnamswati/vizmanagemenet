a02b9e858d2b74439dad0728a7f8bd9a1bf1536b
SHABNAM SWATI <SHABNAM SWATI@LAPTOP-PV5IV72T>
2022-07-19 17:36:22 +0530

    viz management created

---
+++ com.crm.comcast.vizmanagement/src/test/java/forgithub/Createaddress.java
@@ -0,0 +1,19 @@
+package forgithub;
+
+import org.testng.annotations.Test;
+
+public class Createaddress {
+	@Test
+	public void address()
+	{
+		System.out.println("sakchi");
+	}
+		@Test
+		public void Createlocaladdress() 
+		{
+			System.out.println("adityapur");
+		}
+		
+	}
+
+
---
+++ com.crm.comcast.vizmanagement/src/test/java/forgithub/Login.java
@@ -0,0 +1,16 @@
+package forgithub;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test
+
+	public void methodlogin()
+	{
+System.out.println("hii");
+}
+	@Test
+public void logout()
+{
+	System.out.println("by");
+}}
